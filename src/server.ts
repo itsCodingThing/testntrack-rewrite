ab4023b9c5e7183b54aa5599e198f35aa89b0f41
bhanu pratap singh <bhanuparitapsingh09@gmail.com>
2024-10-16 01:22:52 +0530

    update project to support bun

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,8 @@
-import Graceful from "@ladjs/graceful";
 import chalk from "chalk";
 
-import { config } from "project/utils/utils.js";
 import { buildServer } from "project/app/app.js";
 import { prisma } from "project/database/db.connection.js";
+import { config } from "project/utils/utils.js";
 
 /**
  * Connect To DB
@@ -24,15 +23,14 @@ await server.listen({ port: config.server.port, host: config.server.host });
 /**
  * Graceful Listen
  */
-const graceful = new Graceful({
-  servers: [server],
-  customHandlers: [
-    async () => {
-      await prisma.$disconnect();
-    },
-  ],
-});
-graceful.listen();
+// const graceful = new Graceful({
+//   servers: [server],
+//   customHandlers: [
+//     async () => {
+//       await prisma.$disconnect();
+//     },
+//   ],
+// });
+// graceful.listen();
 
 console.log(chalk.bgGreen(`Api running on: http://${config.server.host}:${config.server.port}`));
-console.log(chalk.bgCyan(`enviroment: ${process.env.NODE_ENV}`));
